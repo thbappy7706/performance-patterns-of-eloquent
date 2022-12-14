6e6441f769bce27f1819fee7afecc48b45db7b47
thbappy7706 <tanvir15-7706@diu.edu.bd>
2022-12-15 01:31:45 +0600

    erfe

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,64 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+ <div>
+     <div class="bg-white dark:bg-gray-900">
+         <div class="flex justify-center h-screen">
+             <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)">
+                 <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
+                     <div>
+                         <h2 class="text-4xl font-bold text-white">Hello</h2>
 
-        <x-jet-validation-errors class="mb-4" />
+                         <p class="max-w-xl mt-3 text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus molestiae</p>
+                     </div>
+                 </div>
+             </div>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+             <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
+                 <div class="flex-1">
+                     <div class="text-center">
+                         <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Hello</h2>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+                         <p class="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
+                     </div>
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+                     <div class="mt-8">
+                         <x-jet-validation-errors class="mb-4" />
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+                         @if (session('status'))
+                             <div class="mb-4 font-medium text-sm text-green-600">
+                                 {{ session('status') }}
+                             </div>
+                         @endif
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                         <form method="POST" action="{{ route('login') }}">
+                             @csrf
+                             <div>
+                                 <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
+                                 <input type="email" id="email" name="email" :value="old('email')" placeholder="example@example.com" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+                             <div class="mt-6">
+                                 <div class="flex justify-between mb-2">
+                                     <label for="password" class="text-sm text-gray-600 dark:text-gray-200">Password</label>
+                                     <a href="#" class="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot password?</a>
+                                 </div>
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+                                 <input type="password" name="password" required autocomplete="current-password"class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                             </div>
+
+                             <div class="mt-6">
+                                 <button type="submit"
+                                     class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                     Sign in
+                                 </button>
+                             </div>
+
+                         </form>
+
+                         <p class="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="#" class="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.</p>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
 </x-guest-layout>
